c3eeea7c6be2f2bc5b79936e27c942546565c596
Lautaro Petaccio <lausuper@gmail.com>
2023-11-06 15:44:31 -0300

    fix: Add pages to route

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import 'semantic-ui-css/semantic.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import { UserPage } from './components/Pages/UserPage'
+import { DefaultPage } from './components/Pages/DefaultPage'
+import { CallbackPage } from './components/Pages/CallbackPage'
 import 'decentraland-ui/dist/themes/alternative/dark-theme.css'
 import './index.css'
 
@@ -12,10 +14,10 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<p>Login</p>} />
-        <Route path="/callback" element={<p>Callback</p>} />
-        <Route path="/user" element={<p>User</p>} />
-        <Route path="*" element={<p>Default</p>} />
+        <Route path="/login" element={<p>Login page</p>} />
+        <Route path="/callback" element={<CallbackPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="*" element={<DefaultPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
