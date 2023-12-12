e3ede1e570ea01e70e6d6afb207020292aad097b
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-12-12 17:29:59 -0300

    fix: set basename (#19)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  ...(command === 'build'
+    ? {
+        base: '/profile',
+        optimizeDeps: {
+          esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+              global: 'globalThis'
+            }
+          },
+          build: {
+            commonjsOptions: {
+              transformMixedEsModules: true
+            },
+            sourcemap: true
+          }
+        }
+      }
+    : undefined)
+}))
