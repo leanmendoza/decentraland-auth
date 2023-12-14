95468785c17cae22376e6e1d9a597b2f10b14f0b
Lean Mendoza <leandro@decentraland.org>
2023-12-14 11:39:24 -0300

    fix auth-server

--- src/components/Pages/RemoteWalletPage/config.ts
+++ src/components/Pages/RemoteWalletPage/config.ts
@@ -1,2 +1,2 @@
-// export const AUTH_SERVER_ENDPOINT = 'https://auth-server.dclexplorer.com/task/'
-export const AUTH_SERVER_ENDPOINT = 'http://localhost:5545/task/'
+export const AUTH_SERVER_ENDPOINT = 'https://auth-server.dclexplorer.com/task/'
+// export const AUTH_SERVER_ENDPOINT = 'http://localhost:5545/task/'
